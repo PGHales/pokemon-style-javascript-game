9c0472195b9e13316c16e4ec99800991c96d65e8
Preston Hales <PrestongHales@gmail.com>
2023-07-20 14:30:40 -0600

    Start rendering player

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,27 @@ canvas.height = 576;
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-const map = new Image();
-map.src = '../../assets/img/map.png';
-map.onload = () => ctx.drawImage(map, -2100, -750);
+const map = getImage('map.png');
+const player = getImage('playerDown.png');
+map.onload = () => {
+    ctx.drawImage(map, -2080, -825);
+    ctx.drawImage(
+        player,
+        //Cropping location and width/height
+        0,
+        0,
+        player.width / 4,
+        player.height,
+        //Actual location and width/height
+        canvas.width / 2 - player.width / 8,
+        canvas.height / 2 - player.height / 2,
+        player.width / 4,
+        player.height
+    );
+}
+
+function getImage(name) {
+    const image = new Image();
+    image.src = `../../assets/img/${name}`;
+    return image;
+}
